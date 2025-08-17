eca7141752c887c0a1c47c2ef07d2fdc31b29a72
itune8 <sahilgulmohur@gmail.com>
2025-08-17 23:43:01 +0530

    Start IAM scanner module with severity constants

---
+++ scanners/iam_scanner.py
@@ -0,0 +1,10 @@
+"""IAM (Identity & Access Management) security scanner."""
+
+
+SEVERITY_CRITICAL = "Critical"
+SEVERITY_HIGH = "High"
+SEVERITY_MEDIUM = "Medium"
+SEVERITY_LOW = "Low"
+SEVERITY_INFO = "Info"
+
+IAM_RULES = []
